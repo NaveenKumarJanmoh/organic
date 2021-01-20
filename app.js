66f82315fb17934c6c7b5e98e617b6c0691234ef
Naveen Kumar <n.kpaleti@janmoh.com>
2021-01-20 01:47:00 -0500

    final

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
 var async = require('async');
+var flash = require('connect-flash');
+var router = express.Router();
 
 var connection = mysql.createConnection({
 	host     : 'localhost',
@@ -19,6 +21,7 @@ app.use(session({
 	resave: true,
 	saveUninitialized: true
 }));
+app.use(flash()); 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
@@ -27,4 +30,35 @@ app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+
+app.post('/item/:id', function (request, response) {
+	connection.query("SELECT * FROM organicit where id = ?",[request.params.id], function (error, rows, fields) {
+		if (!!error) {
+			console.log('Error in the query');
+		} else {
+			response.render('item_info', { page_title: "Item",data: rows });
+		}
+	});
+});
+
+app.post('/category/:type', function (request, response) {
+	connection.query("SELECT * FROM organicit where type = ?",[request.params.type], function (error, rows, fields) {
+		if (!!error) {
+			console.log('Error in the query');
+		} else {
+			response.render('items', { page_title: "Item",data: rows });
+		}
+	});
+});
+
+app.post('/cart/:id', function (request, response) {
+	connection.query("insert into org_cart values(?)",[request.params.id], function (error, rows, fields) {
+		if (!!error) {
+			console.log('Error in the query');
+		} else {
+			response.redirect("http://localhost:1000/back.html");
+		}
+	});
+});
+
+app.listen(process.env.PORT || 1000);
---
+++ public/js/account.js
@@ -0,0 +1,173 @@
+
+
+/**
+ * @return {!Object} The FirebaseUI config.
+ */
+function getUiConfig() {
+  return {
+    'callbacks': {
+      // Called when the user has been successfully signed in.
+      'signInSuccessWithAuthResult': function(authResult, redirectUrl) {
+        if (authResult.user) {
+          handleSignedInUser(authResult.user);
+        }
+        if (authResult.additionalUserInfo) {
+          document.getElementById('is-new-user').textContent =
+              authResult.additionalUserInfo.isNewUser ?
+              'New User' : 'Existing User';
+        }
+        // Do not redirect.
+        return false;
+      }
+    },
+    // Opens IDP Providers sign-in flow in a popup.
+    'signInFlow': 'popup',
+    'signInOptions': [
+      // TODO(developer): Remove the providers you don't need for your app.
+      {
+        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        // Required to enable ID token credentials for this provider.
+        clientId: CLIENT_ID
+      },
+      {
+        provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        scopes :[
+          'public_profile',
+          'email',
+          'user_likes',
+          'user_friends'
+        ]
+      },
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      {
+        provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+        recaptchaParameters: {
+          size: getRecaptchaMode()
+        },
+        defaultCountry: 'IN', 
+	      defaultNationalNumber: '1234567890',
+	      loginHint: '+11234567890'
+      },
+      
+    ],
+  };
+}
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// Disable auto-sign in.
+ui.disableAutoSignIn();
+
+
+/**
+ * Redirects to the FirebaseUI widget.
+ */
+var signInWithRedirect = function() {
+  window.location.assign(getWidgetUrl());
+};
+
+
+/**
+ * Open a popup with the FirebaseUI widget.
+ */
+var signInWithPopup = function() {
+  window.open(getWidgetUrl(), 'Sign In', 'width=985,height=735');
+};
+
+
+/**
+ * Displays the UI for a signed in user.
+ * @param {!firebase.User} user
+ */
+var handleSignedInUser = function(user) {
+  document.getElementById('user-signed-in').style.display = 'none';
+  document.getElementById('user-signed-out').style.display = 'none';
+  document.getElementById('button_item_div').style.display = 'block';
+  document.getElementById('profile').style.display = 'block';
+  document.getElementById('sign-out').style.display = 'block';
+  document.getElementById('name').textContent = user.displayName;
+  document.getElementById('email').textContent = user.email;
+  document.getElementById('phone').textContent = user.phoneNumber;
+  if (user.photoURL) {
+    var photoURL = user.photoURL;
+    // Append size to the photo URL for Google hosted images to avoid requesting
+    // the image with its original resolution (using more bandwidth than needed)
+    // when it is going to be presented in smaller size.
+    if ((photoURL.indexOf('googleusercontent.com') != -1) ||
+        (photoURL.indexOf('ggpht.com') != -1)) {
+      photoURL = photoURL + '?sz=' +
+          document.getElementById('photo').clientHeight;
+    }
+    document.getElementById('photo').src = photoURL;
+    document.getElementById('photo').style.display = 'block';
+
+  } else {
+    document.getElementById('photo').style.display = 'none';
+  }
+  
+};
+
+
+/**
+ * Displays the UI for a signed out user.
+ */
+var handleSignedOutUser = function() {
+  //window.location.replace("localhost:1000/account.html");
+  document.getElementById('user-signed-in').style.display = 'none';
+  document.getElementById('user-signed-out').style.display = 'block';
+  document.getElementById('button_item_div').style.display = 'none';
+  document.getElementById('profile').style.display = 'none';
+  document.getElementById('sign-out').style.display = 'none';
+  ui.start('#firebaseui-container', getUiConfig());
+};
+
+// Listen to change in auth state so it displays the correct UI for when
+// the user is signed in or not.
+firebase.auth().onAuthStateChanged(function(user) {
+  document.getElementById('loading').style.display = 'none';
+  document.getElementById('loaded').style.display = 'block';
+  user ? handleSignedInUser(user) : handleSignedOutUser();
+});
+
+/**
+ * Deletes the user's account.
+ */
+var deleteAccount = function() {
+  firebase.auth().currentUser.delete().catch(function(error) {
+    if (error.code == 'auth/requires-recent-login') {
+      // The user's credential is too old. She needs to sign in again.
+      firebase.auth().signOut().then(function() {
+        // The timeout allows the message to be displayed after the UI has
+        // changed to the signed out state.
+        setTimeout(function() {
+          alert('Please sign in again to delete your account.');
+        }, 1);
+      });
+    }
+  });
+};
+
+
+/**
+ * Handles when the user changes the reCAPTCHA or email signInMethod config.
+ */
+function handleConfigChange() {
+  ui.start('#firebaseui-container', getUiConfig());
+}
+
+
+/**
+ * Initializes the app.
+ */
+var initApp = function() {
+  document.getElementById('sign-out').addEventListener('click', function() {
+    firebase.auth().signOut();
+  });
+  document.getElementById('delete-account').addEventListener(
+      'click', function() {
+        deleteAccount();
+      });
+};
+
+window.addEventListener('load', initApp);
